ed0e36967e04327977aa574d8580ec2ee6524f2a
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-01-29 12:05:35 +0530

    changes

--- frontend/src/pages/Config.jsx
+++ frontend/src/pages/Config.jsx
@@ -8,20 +8,26 @@ function Config() {
   const myState = useContext(myContext);
   const { configDta, setConfigData } = myState;
 
-  console.log(myState)
+  // console.log(myState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setConfigData({ ...configDta, [name]: value });
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("submitted")
+    console.log(configDta)
   }
 
   return (
     <div className='config__container'>
       <form onSubmit={ handleSubmit }  className="config__form">
-        <input placeholder='Maximum number of images on canvas..' type="number" required />
-        <input placeholder='Enter background image url..' type="url" required />
-        <input placeholder='Enter main form text..' type="text" required />
-        <input placeholder='Enter main form prompt..' type="text" required />
+        <input onChange={handleChange} placeholder='Maximum number of images on canvas..' type="number" required />
+        <input onChange={handleChange} placeholder='Enter background image url..' type="url" required />
+        <input onChange={handleChange} placeholder='Enter main form text..' type="text" required />
+        <input onChange={handleChange} placeholder='Enter main form prompt..' type="text" required />
 
         
         <button className="submit">Submit</button>
