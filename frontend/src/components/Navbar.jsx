fdfba0aa3fb7261f394724d2025ced3709856fbf
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-02-03 09:54:54 +0530

    changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,54 +6,42 @@ import { useContext, useEffect } from "react";
 // console.log(window.location.pathname)
 
 function Navbar() {
-
   const MyState = useContext(myContext);
   const { isLoading, setIsLoading, localState, setLocalState } = MyState;
 
   // console.log(MyState)
 
   if (isLoading) {
-    return ""
+    return "";
   }
 
   const handleChange = (name, value) => {
-  //  setLocalState((prev) => {...prev, [name]: [value]};
-   setLocalState((prevLocalState) => ({ ...prevLocalState, [name]: value }));
-  }
+    //  setLocalState((prev) => {...prev, [name]: [value]};
+    setLocalState((prevLocalState) => ({ ...prevLocalState, [name]: value }));
+  };
 
   useEffect(() => {
-    const localState = localStorage.getItem('localState');
+    const localState = localStorage.getItem("localState");
     if (localState) {
       const parsedState = JSON.parse(localState);
 
-      handleChange('bg__img', parsedState.bg__img);
-      handleChange('main__form__text', parsedState.main__form__text);
-      handleChange('main__prompt', parsedState.main__prompt);
-      handleChange('home__img', parsedState.home__img);
-      handleChange('theme__color', parsedState.theme__color);
-      handleChange('home__intro', parsedState.home__intro);
+      handleChange("bg__img", parsedState.bg__img);
+      handleChange("main__form__text", parsedState.main__form__text);
+      handleChange("main__prompt", parsedState.main__prompt);
+      handleChange("home__img", parsedState.home__img);
+      handleChange("theme__color", parsedState.theme__color);
+      handleChange("home__intro", parsedState.home__intro);
 
-      console.log(localState)
+      console.log(localState);
       const prsedLocalState = JSON.parse(localState);
-        console.log(prsedLocalState)
-      // console.log(parsedState)
-      // MyState.setBackground(parsedState.bg__img);
-      // MyState.setIntroText(parsedState.main__form__text);
-      // MyState.setPrompt(parsedState.main__prompt);
-      // MyState.setHomeImg(parsedState.home__img);
-      // MyState.setThemeColor(parsedState.theme__color);
-      
-      // MyState.setHomeIntro(parsedState.home__intro);
+      console.log(prsedLocalState);
 
-
-      const navEle = document.querySelector('nav');
+      const navEle = document.querySelector("nav");
       // console.log(navEle);
       navEle.style.backgroundColor = prsedLocalState.theme__color;
       // console.log(localState)
     }
-
-
-  }, [])
+  }, []);
 
   return (
     <nav>
