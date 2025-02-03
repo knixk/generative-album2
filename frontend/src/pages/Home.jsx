f484258287a8b55ed4f7597d802a2697bf1943b7
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-02-03 12:21:41 +0530

    changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import istanbul from "../assets/istanbul.jpg";
 import { myContext } from "../App";
+import toast, { Toaster } from "react-hot-toast";
 // const sampleImg = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtnvAOajH9gS4C30cRF7rD_voaTAKly2Ntaw&s`;
 // const sampleImg2 = `https://img.freepik.com/premium-photo/picture-forest-with-tree-background_198067-945359.jpg`;
 // const sampleImg3 = `https://i.pinimg.com/736x/39/4d/68/394d68ba91ee4f49d400034d8d487997.jpg`;
@@ -11,12 +12,17 @@ function Home() {
   const myState = useContext(myContext);
   const { localState, setLocalState } = myState;
 
-  if (localState) {
+  // if (localState.home__img) {
     console.log(localState);
+  // } else {
+    // toast("go to /config page to set up the app..");
   }
+
+
 
   return (
     <div className="home__container">
+      <Toaster/>
       <img className="home__img" src={localState.home__img} alt="" />
       <h2>{localState.home__intro}</h2>
       <button onClick={() => navigate("/main")}>Get started</button>
